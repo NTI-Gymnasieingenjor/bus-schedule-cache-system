e40958d042b8f31c9584e0e390cc437c79aed657
lividcomet <lividcomet@hotmail.se>
2019-01-21 13:21:56 +0000

    Added new buses for program

--- config.php
+++ config.php
@@ -22,5 +22,20 @@ $config = [
             "start" => "Uppsala Business Park Norra",
             "stop" => "Storvreta Centrum (Uppsala kn)",
         ],
+        [
+            "name" => "1",
+            "start" => "Uppsala Hanselligatan",
+            "stop" => "Uppsala Årstagården",
+        ],
+        [
+            "name" => "10",
+            "start" => "Uppsala Coop/IKEA",
+            "stop" => "Uppsala Bolandcity",
+        ],
+        [
+            "name" => "809",
+            "start" => "Uppsala Business Park södra",
+            "stop" => "Uppsala Kumla",
+        ]
     ],
 ];
